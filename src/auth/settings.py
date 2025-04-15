f8e86f45b89ee7e2710fe88d778497413a3e030d
Arslan <91959072+rasulow@users.noreply.github.com>
2025-04-15 19:15:27 +0500

    change auth port from 8000 to 8082 and change database to postgresql

--- src/auth/settings.py
+++ src/auth/settings.py
@@ -66,20 +66,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'auth.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=env["DATABASE_URL"],
-#         conn_max_age=600,
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=env["DATABASE_URL"],
+        conn_max_age=600,
+    )
+}
 
 AUTH_USER_MODEL = 'account.CustomUser' 
 
